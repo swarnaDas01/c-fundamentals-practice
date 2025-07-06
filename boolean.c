4cb74d96fee2a472e93d402d27f82601ac86b9c9
Swarna Rani Das <66729198+swarnaDas01@users.noreply.github.com>
2025-07-06 20:58:54 +0600

    Add files via upload

---
+++ boolean.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    bool a;
+    a= false;
+    printf("%d", a);
+
+    bool b;
+    b= true;
+    printf("%d", b);
+}
---
+++ data_type.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a= 10;
+    printf("%d\n",a);
+
+    float b= 5.96;
+    printf("%f\n", b);
+
+    // char c= "H"; //Cannot use "h", it should be used when it is a string
+    // printf("%c", c);
+    char c = 'H';  // Store a single character
+    printf("%c\n", c);
+}
