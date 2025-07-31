4c6c73984950a3a24fc5da0b0d0809f9d65b5cc3
Swarna Rani Das <66729198+swarnaDas01@users.noreply.github.com>
2025-07-31 19:16:15 +0600

    Add files via upload

---
+++ T_Shape_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+    int n;
+    scanf("%d", &n);
+    for(int i=n; i>=1; i++)
+    {
+        for(int j=1; j<=n-1; j--)
+        {
+            printf(" ");
+        }
+        for(int k=1; k<=i; k++)
+        {
+            printf("*");
+        }
+    }
+    printf("\n");
+    return 0;
+}
